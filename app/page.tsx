17baa713b85d22cb9a2865bae00dd56b7e8e00f4
Longino Ojeda <longinoojeda@users.noreply.github.com>
2024-02-28 13:24:49 +0100

    Title home updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,24 @@ import { title, subtitle } from "@/components/primitives";
 export default function Home() {
 	return (
 		<section className="flex flex-col md:flex-row items-center justify-between gap-4 ">
-			<div className="inline-block max-w-lg text-center md:text-left">
-				<h1 className={title({size: 'lg'})}>Up Your&nbsp;</h1>
-				<h1 className={title({ color: "nineMindGreen", size: 'lg' })}>Skills&nbsp;</h1>
-				<br />
-				<h1 className={title({size: 'lg'})}>To&nbsp;</h1>
-				<h1 className={title({ color: "nineMindGreen" , size: 'lg'})}>Advance&nbsp;</h1>
-				<h1 className={title({size: 'lg'})}>Your&nbsp;</h1>
-				<br />
-				<h1 className={title({ color: "nineMindGreen" , size: 'lg'})}>Career&nbsp;</h1>
-				<h1 className={title({size: 'lg'})}>Path&nbsp;</h1>
+			<div className="flex flex-col gap-4 max-w-lg text-center md:text-left">
+				<div>
+					<h1 className={title({ size: 'lg' })}>Up Your&nbsp;</h1>
+					<h1 className={title({ color: "nineMindGreen", size: 'lg' })}>Skills&nbsp;</h1>
+				</div>
+				<div>
+					<h1 className={title({ size: 'lg' })}>To&nbsp;</h1>
+					<h1 className={title({ color: "nineMindGreen", size: 'lg' })}>Advance&nbsp;</h1>
+					<h1 className={title({ size: 'lg' })}>Your&nbsp;</h1>
+				</div>
+				<div>
+					<h1 className={title({ color: "nineMindGreen", size: 'lg' })}>Career&nbsp;</h1>
+					<h1 className={title({ size: 'lg' })}>Path&nbsp;</h1>
+				</div>
 				<h2 className={subtitle({ class: "mt-4" })}>
-				Provides you with the latest online learning system and material that help your knowledge growing.
+					Provides you with the latest online learning system and material that help your knowledge growing.
 				</h2>
 			</div>
-
 		</section>
 	);
 }
